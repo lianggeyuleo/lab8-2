bffdfe5ba99196e7ec10fa651cfe2dad2f5dcbcc
LI Junrong <jlibz@WIN2K.CS.UST.HK>
2019-04-11 14:19:37 +0800

    4 commit

--- lab08/src/main/java/Library.java
+++ lab08/src/main/java/Library.java
@@ -3,7 +3,7 @@
  */
 public class Library {
 	public static void main (String arg[]) {
-		System.out.println("Hello everyone!");
+		System.out.println("Hello everyone! jr");
 	}
     public boolean someLibraryMethod() {
         return true;
